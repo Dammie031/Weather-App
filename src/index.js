4d6464d8b31210fecccd4c65c3b699fa693aee64
Dammie031 <115620447+Dammie031@users.noreply.github.com>
2022-10-20 15:22:22 +0100

    Updated time

--- src/index.js
+++ src/index.js
@@ -1,3 +1,51 @@
+function formatDate(timestamp){
+    let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let year = date.getFullYear();
+  console.log(year);
+  let currentDate = date.getDate();
+  console.log(currentDate);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  
+  let day = days[date.getDay()];
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
+  let month = months[date.getMonth()];
+  console.log(month);
+ 
+  return `${day}, ${month} ${currentDate}, ${year} </br> Last updated:${hours}:${minutes}`;
+}
+
+
+
+
 function displayTemperature(response){
     
 let temperatureElement=document.querySelector("#temperature");
@@ -11,12 +59,14 @@ let cityElement=document.querySelector("#city");
 cityElement.innerHTML=response.data.name;
 let descriptionElement=document.querySelector("#description");
 descriptionElement.innerHTML=response.data.weather[0].description;
-
+let dateElement=document.querySelector("#date");
+dateElement.innerHTML=formatDate(response.data.dt * 1000);
 }
 
 
 let apiKey ="9560ff313b286b0ba47037c9fdaafbe3";
-let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+let city="Lagos";
+let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 console.log(apiUrl);
 
 axios.get(apiUrl).then(displayTemperature);
