7c4af3736835a1bee9a52bc65df0c4bb25e9c960
Dammie031 <115620447+Dammie031@users.noreply.github.com>
2022-10-27 16:19:57 +0100

    Complete integration of forecast API

--- src/index.js
+++ src/index.js
@@ -43,6 +43,14 @@ function formatDate(timestamp){
   return `${day}, ${month} ${currentDate}, ${year} </br> Last updated: ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response){
 console.log(response.data.daily);
   let forecast = (response.data.daily);
@@ -52,22 +60,23 @@ console.log(response.data.daily);
 
 let forecastHTML = `<div class="row">`;
 
-forecast.forEach(function (forecastDay) {
+forecast.forEach(function (forecastDay, index) {
+  if (index < 6) {
   forecastHTML = forecastHTML +  
   `
   <div class="col-2">
-    <div class="weather-forecast-date">${forecastDay.time}</div>
+    <div class="weather-forecast-date">${formatDay(forecastDay.time)} </div>
     <img
       src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${forecastDay.condition.icon}.png"
       alt=""
       width="42"
     />
     <div class="weather-forecast-temperatures">
-      <span class="weather-forecast-temperature-max"> ${forecastDay.temperature.maximum}° </span>
-      <span class="weather-forecast-temperature-min"> ${forecastDay.temperature.maximum}° </span>
+      <span class="weather-forecast-temperature-max"> ${Math.round(forecastDay.temperature.maximum)}° </span>
+      <span class="weather-forecast-temperature-min"> ${Math.round(forecastDay.temperature.maximum)}° </span>
     </div>
  </div>`;
-
+  }
 });
 
 
